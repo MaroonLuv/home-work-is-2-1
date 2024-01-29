65fe3ff3958776b3b50768f35d29d31f27b1315c
MaroonLuv <sky_my@list.ru>
2024-01-29 10:55:25 +0300

    Решил простую задачу на конструкции if...elseif

---
+++ homework/main.js
@@ -0,0 +1,14 @@
+const temp = 21;
+
+if (temp >= 30)
+{
+    console.log("Вы можете надеть лёгкую одежжу")
+}
+else if (temp >= 20)
+{
+    console.log("Нужно надеть тёплую одежду")
+}
+else
+{
+    console.log("Нужно надеть зимнюю одежду")
+}
